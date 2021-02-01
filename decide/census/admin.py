c1bb233bed019ecd176416119b4c1c105ad7b641
yoadimpen <yoadimpen@alum.us.es>
2021-02-02 00:27:33 +0100

    lab1: exercise 2 done

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -5,7 +5,7 @@ from .models import Census
 
 class CensusAdmin(admin.ModelAdmin):
     list_display = ('voting_id', 'voter_id', 'adscription')
-    list_filter = ('voting_id', )
+    list_filter = ('voting_id', 'adscription')
 
     search_fields = ('voter_id', )
 
